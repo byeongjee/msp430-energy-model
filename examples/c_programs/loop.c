853b2f88f20df1ba9016d108d2572e2cb454c3c2
Byeongjee Kang <byeongjee.kang@gmail.com>
2025-09-30 16:02:14 -0400

    Simplify example

--- examples/c_programs/loop.c
+++ examples/c_programs/loop.c
@@ -3,35 +3,12 @@
 
 int main(void) {
   initialize();
-  int input_list[11];
   int sum = 0;
   int i;
   for (i = 1; i <= 100; i++) {
     gpio_up();
-
-    input_list[0] = 0;
-    input_list[1] = 1;
-    input_list[2] = 2;
-    input_list[3] = 3;
-    input_list[4] = 4;
-    input_list[5] = 5;
-    input_list[6] = 6;
-    input_list[7] = 7;
-    input_list[8] = 8;
-    input_list[9] = 9;
-    input_list[10] = 10;
+    sum += i;
     gpio_down();
-    input_list[0] = 10;
-    input_list[1] = 9;
-    input_list[2] = 8;
-    input_list[3] = 7;
-    input_list[4] = 9;
-    input_list[5] = 8;
-    input_list[6] = 7;
-    input_list[7] = 6;
-    input_list[8] = 5;
-    input_list[9] = 4;
-    input_list[10] = 3;
   }
   return sum;
 }
