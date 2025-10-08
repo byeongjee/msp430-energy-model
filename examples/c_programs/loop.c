85d6ae96c6f103e7a915e5a439a4f759428af0be
Byeongjee Kang <byeongjee.kang@gmail.com>
2025-10-08 14:31:32 -0400

    Improve loop example

--- examples/c_programs/loop.c
+++ examples/c_programs/loop.c
@@ -1,21 +1,29 @@
 #include "gpio.h"
 #include <msp430.h>
+#include <stdio.h>
 
 int main(void) {
   initialize();
 
-  int sum = 0;
-  int i;
+  int buffer[20];
 
   begin_measurement_window();
 
-  for (i = 1; i <= 10; i++) {
+  for (int i = 0; i < 10; i++) {
     begin_event();
-    sum += i;
+
+    // some random computation
+    buffer[0] = 0;
+    buffer[1] = 1;
+    for (int j = 2; j < 20; j++) {
+      buffer[j] = buffer[j - 1] + buffer[j - 2];
+    }
+
     end_event();
   }
 
   end_measurement_window();
 
-  return sum;
+  int a = buffer[19];
+  return a;
 }
