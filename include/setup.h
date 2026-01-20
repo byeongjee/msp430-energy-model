f64688ddfed1e6e8668a1565d63b84b0bd3b2641
Byeongjee Kang <byeongjee.kang@gmail.com>
2026-01-20 12:25:05 -0500

    Format debug macros in setup.h
    Apply consistent formatting to DEBUG_OUT_* macros using clang-format style.

--- include/setup.h
+++ include/setup.h
@@ -57,63 +57,71 @@ static inline void copy_text_sram(void) {
 // ============================================================================
 
 #if defined(DEBUG) && (DEBUG == 2)
-// Interpreter mode: writes to magic addresses 0x0010-0x001C trigger debug output.
+// Interpreter mode: writes to magic addresses 0x0010-0x001C trigger debug
+// output.
 
-#define DEBUG_OUT_U16(val) do { \
-    uint16_t _dbg_v = (uint16_t)(val); \
-    __asm__ volatile( \
-        "mov %0, r12\n\t" \
-        "mov r12, &0x0010\n\t" \
-        : : "m"(_dbg_v) : "r12", "memory" \
-    ); \
-} while(0)
+#define DEBUG_OUT_U16(val)                                                     \
+  do {                                                                         \
+    uint16_t _dbg_v = (uint16_t)(val);                                         \
+    __asm__ volatile("mov %0, r12\n\t"                                         \
+                     "mov r12, &0x0010\n\t"                                    \
+                     :                                                         \
+                     : "m"(_dbg_v)                                             \
+                     : "r12", "memory");                                       \
+  } while (0)
 
-#define DEBUG_OUT_I16(val) do { \
-    int16_t _dbg_v = (int16_t)(val); \
-    __asm__ volatile( \
-        "mov %0, r12\n\t" \
-        "mov r12, &0x0012\n\t" \
-        : : "m"(_dbg_v) : "r12", "memory" \
-    ); \
-} while(0)
+#define DEBUG_OUT_I16(val)                                                     \
+  do {                                                                         \
+    int16_t _dbg_v = (int16_t)(val);                                           \
+    __asm__ volatile("mov %0, r12\n\t"                                         \
+                     "mov r12, &0x0012\n\t"                                    \
+                     :                                                         \
+                     : "m"(_dbg_v)                                             \
+                     : "r12", "memory");                                       \
+  } while (0)
 
-#define DEBUG_OUT_HEX(val) do { \
-    uint16_t _dbg_v = (uint16_t)(val); \
-    __asm__ volatile( \
-        "mov %0, r12\n\t" \
-        "mov r12, &0x0014\n\t" \
-        : : "m"(_dbg_v) : "r12", "memory" \
-    ); \
-} while(0)
+#define DEBUG_OUT_HEX(val)                                                     \
+  do {                                                                         \
+    uint16_t _dbg_v = (uint16_t)(val);                                         \
+    __asm__ volatile("mov %0, r12\n\t"                                         \
+                     "mov r12, &0x0014\n\t"                                    \
+                     :                                                         \
+                     : "m"(_dbg_v)                                             \
+                     : "r12", "memory");                                       \
+  } while (0)
 
-#define DEBUG_OUT_CHAR(c) do { \
-    uint16_t _dbg_v = (uint16_t)(c); \
-    __asm__ volatile( \
-        "mov %0, r12\n\t" \
-        "mov r12, &0x0016\n\t" \
-        : : "m"(_dbg_v) : "r12", "memory" \
-    ); \
-} while(0)
+#define DEBUG_OUT_CHAR(c)                                                      \
+  do {                                                                         \
+    uint16_t _dbg_v = (uint16_t)(c);                                           \
+    __asm__ volatile("mov %0, r12\n\t"                                         \
+                     "mov r12, &0x0016\n\t"                                    \
+                     :                                                         \
+                     : "m"(_dbg_v)                                             \
+                     : "r12", "memory");                                       \
+  } while (0)
 
-#define DEBUG_OUT_U32(val) do { \
-    uint32_t _dbg_v = (uint32_t)(val); \
-    __asm__ volatile( \
-        "mov %0, r12\n\t" \
-        "mov %1, r13\n\t" \
-        "mov r12, &0x0018\n\t" \
-        "mov r13, &0x001A\n\t" \
-        : : "m"(((uint16_t*)&_dbg_v)[0]), "m"(((uint16_t*)&_dbg_v)[1]) : "r12", "r13", "memory" \
-    ); \
-} while(0)
+#define DEBUG_OUT_U32(val)                                                     \
+  do {                                                                         \
+    uint32_t _dbg_v = (uint32_t)(val);                                         \
+    __asm__ volatile("mov %0, r12\n\t"                                         \
+                     "mov %1, r13\n\t"                                         \
+                     "mov r12, &0x0018\n\t"                                    \
+                     "mov r13, &0x001A\n\t"                                    \
+                     :                                                         \
+                     : "m"(((uint16_t *)&_dbg_v)[0]),                          \
+                       "m"(((uint16_t *)&_dbg_v)[1])                           \
+                     : "r12", "r13", "memory");                                \
+  } while (0)
 
-#define DEBUG_OUT_STR(str) do { \
-    const char* _dbg_v = (const char*)(str); \
-    __asm__ volatile( \
-        "mov %0, r12\n\t" \
-        "mov r12, &0x001C\n\t" \
-        : : "m"(_dbg_v) : "r12", "memory" \
-    ); \
-} while(0)
+#define DEBUG_OUT_STR(str)                                                     \
+  do {                                                                         \
+    const char *_dbg_v = (const char *)(str);                                  \
+    __asm__ volatile("mov %0, r12\n\t"                                         \
+                     "mov r12, &0x001C\n\t"                                    \
+                     :                                                         \
+                     : "m"(_dbg_v)                                             \
+                     : "r12", "memory");                                       \
+  } while (0)
 
 #elif defined(DEBUG) && (DEBUG == 1)
 // Board mode: use printf (requires UART initialization)
